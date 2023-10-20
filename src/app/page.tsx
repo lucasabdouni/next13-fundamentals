3a9acebac0097c45ce967b81c97214b52154bb52
lucasabdouni <lucasabdouni@hotmail.com>
2023-10-20 15:53:24 -0300

    feat: simulated server components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <h1>Hello World</h1>
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('https://api.github.com/users/lucasabdouni')
+  const user = await response.json()
+
+  return (
+    <>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <img src={user.avatar_url} alt="" />
+    </>
+  )
 }
